8f338ba9eda1bfbba75c261e0be1031f30227df3
Matthew Thompson <mattsthomp@gmail.com>
2015-04-07 22:52:30 +0000

    build game loader, add description to Event object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,22 +23,28 @@ def loadGame
     event_stack = g1.css("event") + g2.css("event") + g3.css("event") + g4.css("event")
     Game.create( home_team: game_home_team, visiting_team: game_visiting_team, game_date: game_date )
     event_stack.each do |e|
-      if e.attr("action_type") == 0 and e.attr("msg_type") == 4
-      elsif e.attr("action_type") == 1 and e.attr("msg_type") == 2
-      elsif e.attr("action_type") == 1 and e.attr("msg_type") == 1
-      elsif e.attr("action_type") == 0 and e.attr("msg_type") == 8
-      elsif e.attr("action_type") == 1 and e.attr("msg_type") == 6
-      else  Event.create( )
+      d = e.children.to_s
+      desc = d[10..-6]
+      prd = e.attr("prd").to_i
+      gclock = e.attr("game_clock").to_s
+      if prd < 5 
+        elapsed = (prd*720) - ((gclock[0..1].to_i)*60) - (gclock[3..4].to_i)
+      else
+        elapsed = 2880 + ((prd-4)*300) - ((gclock[0..1].to_i)*60) - (gclock[3..4].to_i)
       end
+      Event.create( event_num: e.attr("eventid"), 
+                    player_code: e.attr("player_code"), 
+                    team: e.attr("tm"), 
+                    home_score: e.attr("htms"), 
+                    visitor_score: e.attr("vtms"), 
+                    event_type: e.attr("msg_type"), 
+                    description: desc,
+                    game_id: "",
+                    time_elapsed: elapsed,)
     end
-#   event_stack.each do |e|
-#      Event.create( event_num: e.attr("eventid"), player_code: e.attr("player_code"), team: e.attr("tm"), home_score: e.attr("htms"), visitor_score: e.attr("vtms") )
-#    end
     render 'game_page/game'
   end
 end
-
-
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
