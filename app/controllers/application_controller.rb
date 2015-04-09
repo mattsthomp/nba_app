3f33da4a69547d4594a76b75fa1f8606e1a0d014
Matthew Thompson <mattsthomp@gmail.com>
2015-04-09 17:39:42 +0000

    build demo mode boxscore builder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,8 +52,8 @@ def runDemo
                        :steve_blake, :chris_kaman, :meyers_leonard, :dorell_wright, :alonzo_gee, :allen_crabbe, :cj_mccollum] 
   visitor_player_array = [:russell_westbrook, :serge_ibaka, :kyle_singler, :nick_collison, :andre_roberson, :dion_waiters, 
                           :mitch_mcgary, :anthony_morrow, :dj_augustin, :perry_jones, :enes_kanter, :jeremy_lamb]
-  empty_stat_hash = { :oboards => 0, :dboards => 0, :assists => 0, :steals => 0, :blocks => 0, :turnovers => 0,
-                      :fouls => 0, :made2pt => 0, :missed2pt => 0, :made3pt => 0,:missed3pt => 0,:madeft => 0,:missedft => 0, :ingame => 0}
+  empty_stat_hash = { :minutes => 0, :oboards => 0, :dboards => 0, :assists => 0, :steals => 0, :blocks => 0, :turnovers => 0,
+                      :fouls => 0, :made2pt => 0, :missed2pt => 0, :made3pt => 0, :missed3pt => 0, :madeft => 0, :missedft => 0, :ingame => 0}
   @players = {}
   home_player_array.each do |p|
     @players[p] = empty_stat_hash.dup
@@ -62,16 +62,103 @@ def runDemo
     @players[p] = empty_stat_hash.dup
   end
   event_array = Event.where(game_id: '20150227/OKCPOR').first(play_count)
-  event_array.each do |e|
-    if e[:event_type] == 4
-      reb_player = e[:player_code].to_sym
-      if reb_player == ""
-        # count team rebounds?
-      else
-        if @players[reb_player]
-          @players[reb_player][:dboards] += 1
+  event_array.each do |e|   
+    first_player = e[:player_code].to_sym
+    if e[:event_type] == 2   # -----------------------------------------------------------missed shots and blocks
+      if e[:description].include?("Block:")
+        x = e[:description].split("Block:")
+        y = x[1].split("(")
+        z = y[0].strip.downcase
+        if e[:team] == "thunder"
+          home_player_array.each do |h|
+            if z == h.to_s.split("_")[1]
+              @players[h][:blocks] += 1
+            end
+          end
+        else  
+          visitor_player_array.each do |v|
+            if z == v.to_s.split("_")[1]
+              @players[v][:blocks] += 1
+            end
+          end
         end
       end
+      if e[:description].include?("3pt")
+        @players[first_player][:missed3pt] += 1
+      else
+        @players[first_player][:missed2pt] += 1
+      end
+    elsif e[:event_type] == 4   # -----------------------------------------------------------rebounds
+      if first_player == :""
+        # count team rebounds?
+      else
+        @players[first_player][:dboards] += 1
+      end
+    elsif e[:event_type] == 1  # -----------------------------------------------------------made shots and assists
+      if e[:description].include?("Assist:")
+        x = e[:description].split("Assist:")
+        y = x[1].split("(")
+        z = y[0].strip.downcase
+        if e[:team] == "blazers"
+          home_player_array.each do |h|
+            if z == h.to_s.split("_")[1]
+              @players[h][:assists] += 1
+            end
+          end
+        else  
+          visitor_player_array.each do |v|
+            if z == v.to_s.split("_")[1]
+              @players[v][:assists] += 1
+            end
+          end
+        end
+      end
+      if e[:description].include?("3pt")
+        @players[first_player][:made3pt] += 1
+      else
+        @players[first_player][:made2pt] += 1
+      end
+    elsif e[:event_type] == 3  # -----------------------------------------------------------free throws
+      if e[:description].include?("Missed")
+        @players[first_player][:missedft] += 1
+      else
+        @players[first_player][:madeft] += 1
+      end
+    elsif e[:event_type] == 6  # -----------------------------------------------------------fouls
+      if e[:description].include?("Technical")
+      else
+        @players[first_player][:fouls] += 1
+      end
+    elsif e[:event_type] == 5  # -----------------------------------------------------------turnovers and steals
+       if e[:description].include?("Steal:")
+         x = e[:description].split("Steal:")
+         y = x[1].split("(")
+         z = y[0].strip.downcase
+         if e[:team] == "thunder"
+          home_player_array.each do |h|
+            if z == h.to_s.split("_")[1]
+              @players[h][:steals] += 1
+            end
+          end
+          else  
+           visitor_player_array.each do |v|
+             if z == v.to_s.split("_")[1]
+              @players[v][:steals] += 1
+             end
+           end
+         end
+       end
+       if first_player == :""
+         # - team turnover
+       else
+         @players[first_player][:turnovers] += 1
+       end
+    elsif e[:event_type] == 8  # -----------------------------------------------------------subs
+    elsif e[:event_type] == 9  # -----------------------------------------------------------timeouts
+    elsif e[:event_type] == 12  # -----------------------------------------------------------start period
+    elsif e[:event_type] == 13  # -----------------------------------------------------------end period
+    elsif e[:event_type] == 7  # -----------------------------------------------------------violations
+    elsif e[:event_type] == 10  # -----------------------------------------------------------jump ball
     end
   end
   @counter = play_count.to_s
